aaf6b91ae20a8d0d7dfa35f37020dab03e5343f9
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-05-08 22:30:47 -0700

    added server and package.json

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
