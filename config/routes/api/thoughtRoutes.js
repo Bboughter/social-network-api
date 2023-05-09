ff142933d79820d959b3ecc1f5ee2d8502afad1f
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-05-08 23:29:02 -0700

    thoughtRoutes added to api routes

---
+++ config/routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+const router = express.Router();
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
