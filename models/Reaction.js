282c49df2cbae918c3dcd98398cf964f095ea161
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-05-08 22:59:35 -0700

    reaction.js file added to models

---
+++ models/Reaction.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 255,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => new Date(timestamp).toISOString(),
+    },
+});
+
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+module.exports = Reaction;
